3f6e44478853e3ac723de10e44090eef867f2ab5
Manzoor Sofi <sofimanzoor888@gmail.com>
2023-07-07 08:32:40 +0530

    other functions in landing-page

--- jss/index.js
+++ jss/index.js
@@ -5,23 +5,30 @@ galleryElements.forEach(function (element) {
 		document.querySelector(".parents").src = e.target.getAttribute("src");
 	});
 	element.addEventListener("mouseout", function (e) {
-		document.querySelector(".parents").src = "/images/landing.png";
+		document.querySelector(".parents").src =
+			document.getElementById("current_image").value;
 	});
 });
 
-// var galleryElements = document.querySelectorAll(".first-1");
+// on-click setting bg image permenant
+function testclick(value) {
+	document.querySelector(".parents").src = value;
+	document.getElementById("current_image").value = value;
+}
 
-// galleryElements.forEach(function (element) {
-// 	element.addEventListener("mouseover", function (e) {
-// 		document.querySelector(".parents").src = e.target.getAttribute("src");
-// 	});
+// for pre-loader
+window.addEventListener("load", function () {
+	const preloader = document.getElementById("preloader");
+	preloader.style.display = "none";
+});
 
-// 	element.addEventListener("mouseout", function (e) {
-// 		var landingImage = e.target.getAttribute("data-landing-image");
-// 		document.querySelector(".parents").src =
-// 			landingImage || "/images/landing.png";
-// 	});
-// });
-// var dynamicImageUrl = "{{ mall1.file_name.url }}";
-// document.getElementById("dynamic-bg").style.backgroundImage =
-// 	"url(" + dynamicImageUrl + ")";
+// for changing text-1
+document.querySelector(".first-3").addEventListener("click", function () {
+	document.querySelector(".text-1").textContent =
+		"Avenue Mall at Brigade Gateway";
+});
+
+document.querySelector(".first-4").addEventListener("click", function () {
+	document.querySelector(".text-1").textContent =
+		"Uptown Mall at Brigade Gateway";
+});
